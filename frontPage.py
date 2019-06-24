e079967a60a6a6bac001ec58572980af6e00a20d
Harshil Gupta <43322542+Harshilgupta95@users.noreply.github.com>
2019-06-24 14:28:17 +0530

    Update frontPage.py

--- frontPage.py
+++ frontPage.py
@@ -1,22 +1,30 @@
 from tkinter import *
-from playsound import playsound
+#from playsound import playsound
 import os
+from datetime import datetime;
 root=Tk()
 
-root.title('Face Recognizer')
 root.configure(background='white')
 
 def capRec():
-    os.system('python captureRecord.py')
+    os.system('C:\\Users\\LENOVO\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe captureRecord.py')
 
 def train():
-    os.system('python training.py')
+    os.system('C:\\Users\\LENOVO\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe training.py')
 
 def det():
-    os.system('python Detect.py')
+    os.system('C:\\Users\\LENOVO\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe Detect.py')
 
-button = Button(root, text='Capture Record', width=25, command=capRec())
-button = Button(root, text='Capture Record', width=25, command=capRec())
-button = Button(root, text='Capture Record', width=25, command=capRec())
+def exit():
+    root.destroy()
+
+root.title('Face Recognizer')
+Label(root,text='Face Recognizer',font=("times new roman",20),fg="white",bg="maroon",height=2).grid(row=0,rowspan=2,columnspan=2,sticky=N+E+W+S,padx=5,pady=5)
+
+Button(root, text='Capture Record',bg="#0D47A1",fg="white",command=capRec).grid(row=3,columnspan=2,sticky=W+E+N+S,padx=5,pady=5)
+Button(root, text='Train Model',bg="#0D47A1",fg="white",command=train).grid(row=4,columnspan=2,sticky=W+E+N+S,padx=5,pady=5)
+Button(root, text='Recognizer',bg="#0D47A1",fg="white",command=det).grid(row=5,columnspan=2,sticky=W+E+N+S,padx=5,pady=5)
+Button(root, text='Exit',bg="#0D47A1",fg="white",command=exit).grid(row=6,columnspan=2,sticky=W+E+N+S,padx=5,pady=5)
+
 
 root.mainloop()
